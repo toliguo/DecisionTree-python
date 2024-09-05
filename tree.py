b9b6e11cf579a0321c6264dd20af3fdcda25ef9c
Guo Li <li.guo@live.com>
2024-09-05 11:16:41 +0800

    update readme

--- tree.py
+++ tree.py
@@ -2,11 +2,147 @@
 
 from math import log
 import operator
-import treePlotter
+
 from collections import Counter
 
 pre_pruning = True
 post_pruning = True
+
+import matplotlib.pyplot as plt
+from pylab import mpl
+import matplotlib.font_manager as fm
+
+font_path = './SimHei.ttf'
+fm.fontManager.addfont(font_path)
+
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', \
+                            xytext=centerPt, textcoords='axes fraction', \
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = getTreeDepth(secondDict[key]) + 1
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalw, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalw
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalw = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalw
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    # plt.show()
+
+
+# ID3决策树
+def ID3_Tree(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalw = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalw
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.title("ID3决策树", fontsize=12, color='red')
+    # plt.show()
+    plt.savefig('pic_results/figure_ID3.png')
+    print("---------------------------------------------")
+    print("png saved: pic_results/figure_ID3.png")
+
+
+# C4.5决策树
+def C45_Tree(inTree):
+    fig = plt.figure(2, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalw = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalw
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.title("C4.5决策树", fontsize=12, color='red')
+    # plt.show()
+    plt.savefig('pic_results/figure_C4.5.png')
+    print("---------------------------------------------")
+    print("png saved: pic_results/figure_C4.5.png")
+
+
+# CART决策树
+def CART_Tree(inTree):
+    fig = plt.figure(3, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalw = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalw
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.title("CART决策树", fontsize=12, color='red')
+    # plt.show()
+    plt.savefig('pic_results/figure_CART.png')
+    print("---------------------------------------------")
+    print("png saved: pic_results/figure_CART.png")
 
 
 def read_dataset(filename):
@@ -432,7 +568,7 @@ if __name__ == '__main__':
     filename = 'dataset.txt'
     testfile = 'testset.txt'
     dataset, labels = read_dataset(filename)
-    # dataset,features=createDataSet()
+
     print('dataset', dataset)
     print("---------------------------------------------")
     print(u"数据集长度", len(dataset))
@@ -466,8 +602,7 @@ if __name__ == '__main__':
             labels_tmp = labels[:]  # 拷贝，createTree会改变labels
             ID3desicionTree = ID3_createTree(dataset, labels_tmp, test_dataset=read_testset(testfile))
             print('ID3desicionTree:\n', ID3desicionTree)
-            # treePlotter.createPlot(ID3desicionTree)
-            treePlotter.ID3_Tree(ID3desicionTree)
+            ID3_Tree(ID3desicionTree)
             testSet = read_testset(testfile)
             print("---------------------------------------------")
             print("下面为 ID3_TestSet_classifyResult：")
@@ -479,7 +614,7 @@ if __name__ == '__main__':
             labels_tmp = labels[:]  # 拷贝，createTree会改变labels
             C45desicionTree = C45_createTree(dataset, labels_tmp, test_dataset=read_testset(testfile))
             print('C45desicionTree:\n', C45desicionTree)
-            treePlotter.C45_Tree(C45desicionTree)
+            C45_Tree(C45desicionTree)
             testSet = read_testset(testfile)
             print("---------------------------------------------")
             print("下面为 C4.5_TestSet_classifyResult:")
@@ -491,7 +626,7 @@ if __name__ == '__main__':
             labels_tmp = labels[:]  # 拷贝，createTree会改变labels
             CARTdesicionTree = CART_createTree(dataset, labels_tmp, test_dataset=read_testset(testfile))
             print('CARTdesicionTree:\n', CARTdesicionTree)
-            treePlotter.CART_Tree(CARTdesicionTree)
+            CART_Tree(CARTdesicionTree)
             testSet = read_testset(testfile)
             print("---------------------------------------------")
             print("下面为 CART_TestSet_classifyResult:")
